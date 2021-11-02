bfa1016f4ab2707aaeb822689e067b5cf762019f
Vp <vp2b2t@gmail.com>
2021-11-02 23:07:57 +0200

    Hello world and variables

---
+++ Variables/variables.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    string name = "John";
+    int age;
+    age = 25;
+    string game = "Minecraft";
+
+    cout << "Hello my name is " << name << "!" << endl;
+    cout << "Im " << age << " Years old." << endl;
+    cout << "I like to play " << game << "." << endl;
+    cout << "Thanks!" << endl;
+
+    return 0;
+}
+
